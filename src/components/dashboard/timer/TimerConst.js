f5e3c9448611ff390ba5d7e175c6ea9046039685
brandonyeoxg <brandonyeoxg@gmail.com>
2019-10-05 20:07:01 +0800

    Refactor out magic numbers with TimerConst

---
+++ src/components/dashboard/timer/TimerConst.js
@@ -0,0 +1,33 @@
+const AVOCADORO_TIME = 10;
+const SHORT_BREAK_TIME = 3;
+const LONG_BREAK_TIME = 900;
+
+export class TimeConst {
+  static get AVOCADORO_TIME() {
+    return AVOCADORO_TIME;
+  }
+
+  static get SHORT_BREAK_TIME() {
+    return SHORT_BREAK_TIME;
+  }
+
+  static get LONG_BREAK_TIME() {
+    return LONG_BREAK_TIME;
+  }
+}
+
+const timerState = Object.freeze({AVOCADORO: 1, SHORT_BREAK: 2, LONG_BREAK: 3})
+
+export class TimerState {
+  static get AVOCADORO() {
+    return timerState.AVOCADORO
+  }
+
+  static get SHORT_BREAK() {
+    return timerState.SHORT_BREAK
+  }
+
+  static get LONG_BREAK() {
+    return timerState.LONG_BREAK
+  }
+}
