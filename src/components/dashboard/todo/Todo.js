540448cb075641cdeef4b4076397c742a7996296
brandonyeoxg <brandonyeoxg@gmail.com>
2019-10-23 09:32:13 +0800

    Add todolist feature

--- src/components/dashboard/todo/Todo.js
+++ src/components/dashboard/todo/Todo.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from './TodoList';
 import InProgress from './InProgress';
 import Completed from './Completed';
 
 const Todo = () => {
+  const [todoTasks, addTodo] = useState([]);
   return(
     <div>
       <InProgress/>
-      <TodoList/>
+      <TodoList todoTasks={todoTasks} addTodo={addTodo}/>
       <Completed/>
     </div>
   );
--- src/components/dashboard/todo/TodoList.js
+++ src/components/dashboard/todo/TodoList.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoCard from './TodoCard';
 
-const TodoList = () => {
+const TodoList = (props) => {
+  const [todoTasks, setTodoTasks] = useState(props.todoTasks)
+  const onClickFunc = (e) => {
+    const currentTodo = props.todoTasks
+    const id = currentTodo.length
+    const todo = {id: id, content:"hello"}
+    props.addTodo([...currentTodo, todo])
+    setTodoTasks([...todoTasks, todo])
+  }
+
+  const showTodoList = todoTasks.map(todo => <TodoCard key={todo.id}/>)
   return(
     <div>
-      <span>Todo</span>
-      <TodoCard/>
-      <TodoCard/>
-      <TodoCard/>
+      <span>Todo  
+        <a className="btn-floating btn-small waves-effect waves-light red" onClick={onClickFunc}><i className="material-icons">add</i></a>
+      </span>
+      {showTodoList}
     </div>
   );
 };
