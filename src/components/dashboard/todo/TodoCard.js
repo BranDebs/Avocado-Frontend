4a781422deff36cd3f82e29513f593c27686a655
brandonyeoxg <brandonyeoxg@gmail.com>
2019-10-24 09:03:13 +0800

    Add hover of card styling

--- src/components/dashboard/todo/TodoCard.js
+++ src/components/dashboard/todo/TodoCard.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const defaultStyle = {
+  card: "card blue-grey darken-1",
+  content: "card-content white-text",
+}
+
+const hoveredStyle = {
+  card:"card green darken-3",
+  content: "card-content white-text",
+}
 
 const TodoCard = () => {
+  const [hoverStyle, setHoverStyle] = useState(defaultStyle)
   function onHover(e) {
     console.log("Got to change colour here")
+    setHoverStyle(hoveredStyle)
   }
   function onOut(e) {
     console.log("Got to change back to default colour")
+    setHoverStyle(defaultStyle)
   }
   return(
-    <div className="card blue-grey darken-1" onMouseOver={() => onHover()} onMouseOut={() => onOut()}>
-      <div className="card-content white-text">
+    <div className={hoverStyle.card} onMouseOver={() => onHover()} onMouseOut={() => onOut()}>
+      <div className={hoverStyle.content}>
         <span className="card-title">Card Title</span>
         <p>lorem ipsum</p>
       </div>
