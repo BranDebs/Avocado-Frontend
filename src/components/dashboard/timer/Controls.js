9bd971e8c90f6cc4c351661beeccb596a35b4af2
deborahlow97 <deborahlow97@gmail.com>
2019-12-18 00:53:25 +0800

    refactor(bug): fix timer buttons

--- src/components/dashboard/timer/Controls.js
+++ src/components/dashboard/timer/Controls.js
@@ -31,24 +31,18 @@ const Controls = props => {
   };
 
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
-      <Button variant={'contained'}>Start</Button>
-      <Button variant={'contained'}>Pause</Button>
-      <Button variant={'contained'}>Reset</Button>
+      <Button variant={'contained'} name="start" onClick={onClickFunc}>
+        Start
+      </Button>
+      <Button variant={'contained'} name="pause" onClick={onClickFunc}>
+        Pause
+      </Button>
+      <Button variant={'contained'} name="reset" onClick={onClickFunc}>
+        Reset
+      </Button>
     </div>
-    // <div className="row controls-container">
-    //   <div className="col s2 offset-s3">
-    //     <a className="waves-effect waves-light btn" name="start" onClick={onClickFunc}>Start</a>
-    //   </div>
-    //   <div className="col s2">
-    //     <a className="waves-effect waves-light btn" name="pause" onClick={onClickFunc}>Pause</a>
-    //   </div>
-    //   <div className="col s2">
-    //     <a className="waves-effect waves-light btn" name="reset" onClick={onClickFunc}>Reset</a>
-    //   </div>
-    // </div>
   );
 };
 
--- src/components/dashboard/todo/TodoCard.js
+++ src/components/dashboard/todo/TodoCard.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
 
 const defaultStyle = {
   card: 'card blue-grey darken-1',
@@ -28,6 +29,9 @@ const TodoCard = () => {
   return (
     <Card>
       <CardContent>
+        <IconButton component="span" style={{ flex: 1 }}>
+          <EditIcon fontSize={'small'} />
+        </IconButton>
         <Typography>Lorem ipsummmmmie</Typography>
       </CardContent>
     </Card>
