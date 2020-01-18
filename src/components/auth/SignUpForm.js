2af4ad919f544a9116d8e3c330a262e4844366ee
deborahlow97 <deborahlow97@gmail.com>
2020-01-18 22:33:25 +0800

    feat(auth-forms): add and update auth forms UI

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -1,7 +1,155 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import './auth.css';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import validateInput from 'server/validations/ValidateLogin';
+import Box from '@material-ui/core/Box';
 
 function SignUpForm() {
-  return <div>I am signup</div>;
+  const [values, setValues] = React.useState({
+    username: '',
+    emailAddress: '',
+    password: '',
+    retypePassword: '',
+    identifier: '',
+    errors: {},
+    isLoading: false
+  });
+  const useStyles = makeStyles(theme => ({
+    root: {
+      padding: theme.spacing(3, 2)
+    },
+    paper: {
+      paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(3)
+    },
+    textfield: {
+      paddingTop: 10,
+      paddingBottom: 10
+    },
+    title: {
+      fontSize: 32,
+      paddingTop: 10,
+      paddingBottom: 10
+    }
+  }));
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  const isValid = () => {
+    const { errors, isValid } = validateInput(values);
+    if (!isValid) {
+      setValues({ ...values, errors: errors });
+    }
+    return isValid;
+  };
+
+  // onsubmit(e) {
+  //   e.preventDefault();
+  //
+  // }
+  const classes = useStyles();
+
+  return (
+      <Grid container>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <div className={classes.paper}>
+            <Paper className="auth-paper">
+              <Typography component="h3" className={classes.title}>
+                <Box fontWeight="fontWeightBold">
+                  Sign Up
+                </Box>
+              </Typography>
+              <FormControl fullWidth className={classes.textfield}>
+                <InputLabel id="username" label="Username">
+                  Username
+                </InputLabel>
+                <Input
+                    value={values.username}
+                    error={values.errors.username}
+                    onChange={handleChange('username')}
+                />
+              </FormControl>
+              <FormControl fullWidth className={classes.textfield}>
+                <InputLabel id="emailAddress" label="Email Address">
+                  Email Address
+                </InputLabel>
+                <Input
+                    value={values.emailAddress}
+                    error={values.errors.emailAddress}
+                    onChange={handleChange('emailAddress')}
+
+                />
+              </FormControl>
+              <FormControl fullWidth className={classes.textfield}>
+                <InputLabel id="password" label="Password">
+                  Password
+                </InputLabel>
+                <Input
+                    type={'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    error={values.errors.password}
+                />
+              </FormControl>
+              <FormControl fullWidth className={classes.textfield}>
+                <InputLabel id="password" label="Password">
+                  Password
+                </InputLabel>
+                <Input
+                    type={'password'}
+                    value={values.retypePassword}
+                    onChange={handleChange('retypePassword')}
+                    error={values.errors.retypePassword}
+                />
+              </FormControl>
+              <div className={classes.paper}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    disabled={values.isLoading}
+                >
+                  Create Account
+                </Button>
+              </div>
+              <Typography>or</Typography>
+              <div className={classes.paper}>
+                <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                >
+                  Sign Up with a Google Account
+                </Button>
+              </div>
+            </Paper>
+          </div>
+        </Grid>
+      </Grid>
+  );
 }
 
 export default SignUpForm;
