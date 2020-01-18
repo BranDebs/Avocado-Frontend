d842eb5a0e5d0a8c12a2a5e3590e67f9c1a11e01
deborahlow97 <deborahlow97@gmail.com>
2020-01-18 15:53:00 +0800

    refactor(card-ui): refactor styling code

--- src/components/dashboard/todo/TodoCard.js
+++ src/components/dashboard/todo/TodoCard.js
@@ -7,7 +7,6 @@ import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
-import todo from './Todo.css';
 import { makeStyles } from '@material-ui/core/styles';
 
 const defaultStyle = {
@@ -62,7 +61,7 @@ const TodoCard = () => {
   };
 
   const useStyles = makeStyles(theme => ({
-    cardStyle: {
+    cardContentStyle: {
       paddingTop: 0,
       paddingRight: 5
     },
@@ -80,17 +79,20 @@ const TodoCard = () => {
     },
     overflow: {
       overflowWrap: 'break-word'
+    },
+    cardStyle: {
+      width: 'inherit'
     }
   }));
   const classes = useStyles();
 
   return (
-    <Card style={{ width: 'inherit' }}>
-      <CardContent className={classes.cardStyle}>
-        <IconButton component="span" style={{ flex: 1 }} onClick={handleIsEdit}>
+    <Card className={classes.cardStyle}>
+      <CardContent className={classes.cardContentStyle}>
+        <IconButton component="span" onClick={handleIsEdit}>
           <EditIcon className={classes.iconSize} />
         </IconButton>
-        <IconButton component="span" style={{ flex: 1 }}>
+        <IconButton component="span">
           <DeleteIcon className={classes.iconSize} />
         </IconButton>
         {showTodoCard()}
@@ -98,7 +100,6 @@ const TodoCard = () => {
           <Button
             variant="contained"
             component="span"
-            align={'right'}
             className={classes.buttonStyle}
           >
             Archive
