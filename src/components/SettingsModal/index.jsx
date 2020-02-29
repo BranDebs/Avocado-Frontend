936a6b249300d328b063eaa89f28a963f4e3c4bc
deborahlow97 <deborahlow97@gmail.com>
2020-02-29 22:38:06 +0800

    fix(bug): changed path name

--- src/components/SettingsModal/index.jsx
+++ src/components/SettingsModal/index.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import Settings from 'components/settings';
+import Settings from 'components/Settings';
 
 const SettingsModal = ({ settings, onSettings, onSave }) => {
   const useStyles = makeStyles(theme => ({
