a68dfbd9b9d1455e62114c9e8c14d4ac00417e3f
brandonyeoxg <brandonyeoxg@gmail.com>
2020-03-14 11:34:35 +0800

    refactor(Timer): refactor out Selections component
    Selection component is tightly coupled with the timer so should not be a
    component by itself.

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import Selections from './Selections';
 import Controls from './Controls';
-import { TimerState } from 'const/timer';
+import {
+  TimerState,
+  AVOCADORO,
+  SHORT_BREAK,
+  LONG_BREAK,
+  AVOCOMBO
+} from 'const/timer';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import './index.css';
 import { formatTimer } from 'utils/timerUtil';
 import { playAlarm } from 'utils/audio';
 
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
+
 const Timer = ({ settings }) => {
-  const [initTimer, setInitTimer] = useState(settings.settings.avocadoDur);
+  const [initTimer, setInitTimer] = useState(settings.avocadoDur);
   const [timer, setTimer] = useState(initTimer);
   const [hasStarted, setTimerToggle] = useState(false);
   const [timerState, setTimerState] = useState(TimerState.AVOCADORO);
@@ -42,30 +59,30 @@ const Timer = ({ settings }) => {
       if (timer === 0 && avocombo === true) {
         switch (timerState) {
           case TimerState.AVOCADORO:
-            setTimer(settings.settings.shortBreakDur);
-            setInitTimer(settings.settings.shortBreakDur);
+            setTimer(settings.shortBreakDur);
+            setInitTimer(settings.shortBreakDur);
             setTimerState(TimerState.SHORT_BREAK);
-            setTimerToggle(settings.settings.autoStartAvocadoro);
+            setTimerToggle(settings.autoStartAvocadoro);
             setAvocomboCount(avoComboCount + 1);
             if (avoComboCount >= 4) {
-              setTimer(settings.settings.longBreakDur);
-              setInitTimer(settings.settings.longBreakDur);
+              setTimer(settings.longBreakDur);
+              setInitTimer(settings.longBreakDur);
               setTimerState(TimerState.LONG_BREAK);
-              setTimerToggle(settings.settings.autoStartBreak);
+              setTimerToggle(settings.autoStartBreak);
               setAvocomboCount(0);
             }
             break;
           case TimerState.SHORT_BREAK:
-            setTimer(settings.settings.avocadoDur);
-            setInitTimer(settings.settings.avocadoDur);
+            setTimer(settings.avocadoDur);
+            setInitTimer(settings.avocadoDur);
             setTimerState(TimerState.AVOCADORO);
-            setTimerToggle(settings.settings.autoStartBreak);
+            setTimerToggle(settings.autoStartBreak);
             break;
           case TimerState.LONG_BREAK:
-            setTimer(settings.settings.avocadoDur);
-            setInitTimer(settings.settings.avocadoDur);
+            setTimer(settings.avocadoDur);
+            setInitTimer(settings.avocadoDur);
             setTimerState(TimerState.AVOCADORO);
-            setTimerToggle(settings.settings.autoStartBreak);
+            setTimerToggle(settings.autoStartBreak);
             break;
         }
       }
@@ -75,20 +92,95 @@ const Timer = ({ settings }) => {
     return () => clearInterval(interval);
   }, [hasStarted, timer]);
 
+  const onSelection = e => {
+    switch (e.currentTarget.name) {
+      case AVOCADORO:
+        setTimer(settings.avocadoDur);
+        setInitTimer(settings.avocadoDur);
+        setTimerState(TimerState.AVOCADORO);
+        break;
+      case SHORT_BREAK:
+        setTimer(settings.shortBreakDur);
+        setInitTimer(settings.shortBreakDur);
+        setTimerState(TimerState.SHORT_BREAK);
+        break;
+      case LONG_BREAK:
+        setTimer(settings.longBreakDur);
+        setInitTimer(settings.longBreakDur);
+        setTimerState(TimerState.LONG_BREAK);
+        break;
+      case AVOCOMBO:
+        setTimer(settings.avocadoDur);
+        setInitTimer(settings.avocadoDur);
+        setTimerState(TimerState.AVOCADORO);
+        setAvocombo(!avocombo);
+        break;
+      default:
+        setTimer(0);
+        setInitTimer(0);
+        return;
+    }
+    setTimerToggle(true);
+  };
+
+  const Selections = () => {
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+    const useStyles = makeStyles(theme => ({
+      tab: {
+        minWidth: 115
+      }
+    }));
+    const classes = useStyles();
+
+    return (
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          variant="scrollable"
+          onChange={handleChange}
+          scrollButtons="auto"
+        >
+          <Tab
+            label="Avocadoro"
+            name={AVOCADORO}
+            className={classes.tab}
+            onClick={onSelection}
+            {...a11yProps(0)}
+          />
+          <Tab
+            label="Short Break"
+            name={SHORT_BREAK}
+            className={classes.tab}
+            onClick={onSelection}
+            {...a11yProps(1)}
+          />
+          <Tab
+            label="Long Break"
+            name={LONG_BREAK}
+            className={classes.tab}
+            onClick={onSelection}
+            {...a11yProps(2)}
+          />
+          <Tab
+            label="Avocombo"
+            name={AVOCOMBO}
+            className={classes.tab}
+            onClick={onSelection}
+          />
+        </Tabs>
+      </AppBar>
+    );
+  };
+
   return (
     <Paper>
       <Grid container>
         <Grid item xs={12}>
-          <Selections
-            settings={settings.settings}
-            setTimerFunc={setTimer}
-            setInitTimerFunc={setInitTimer}
-            setTimerToggleFunc={setTimerToggle}
-            setTimerStateFunc={setTimerState}
-            setAvocomboFunc={setAvocombo}
-            initTimer={initTimer}
-            avocombo={avocombo}
-          />
+          {Selections()}
         </Grid>
         <Grid item xs={12} className={'timer-style'}>
           {avocount}
