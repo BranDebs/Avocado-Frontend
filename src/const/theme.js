32fc8877a4357906a11f52db1943ce548cefe534
deborahlow97 <deborahlow97@gmail.com>
2020-04-09 21:32:42 +0800

    feat(colour): Add colour palette to project

---
+++ src/const/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const THEME = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffc0cb'
+    },
+    secondary: {
+      main: '#9a0441'
+    }
+  }
+});
